a70b435fa1385e5f2ccb3c92d984bb36c2e2d041
FabioDiCeglie <dcfabio96@gmail.com>
2022-05-09 12:35:07 +0200

    styled component

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,4 +1,46 @@
 /* eslint-disable import/no-anonymous-default-export */
-export default () => {
-  return <div></div>;
+import styled from "styled-components";
+
+type LayoutProps = {
+  gap: number;
 };
+
+const Row = styled.div<LayoutProps>`
+  display: flex;
+  flex-direction: row;
+  gap: ${(props) => props.gap}px;
+`;
+
+const Column = styled.div<LayoutProps>`
+  display: flex;
+  flex-direction: column;
+  gap: ${(props) => props.gap}px;
+`;
+
+function Game() {
+  return (
+    <Row gap={20}>
+      <Column gap={20}>
+        <div>Status</div>
+        <Board />
+      </Column>
+      <Log />
+    </Row>
+  );
+}
+
+function Board() {
+  return <div>Board</div>;
+}
+
+function Log() {
+  return (
+    <ol>
+      <li>
+        <button>Go to move</button>
+      </li>
+    </ol>
+  );
+}
+
+export default Game;
